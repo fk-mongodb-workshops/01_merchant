3b9a8e7c3d0ccecb3a9aad278b7df959dd181269
Fernando Karnagi <fkarnagi@gmail.com>
2025-04-18 07:04:22 +0800

    Update

--- story_teller/fn_linkaja_customer_refuels.js
+++ story_teller/fn_linkaja_customer_refuels.js
@@ -1,7 +1,7 @@
 import moment from "moment";
-import { VoyageAIClient } from "voyageai";
 import { MongoClient } from "mongodb";
 import 'dotenv/config';
+import axios from 'axios';
 
 const exportFn = async function (arg) {
   const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.0pftinh.mongodb.net/`;
@@ -14,12 +14,33 @@ const exportFn = async function (arg) {
   var dbName = "linkaja";
   var collName = "trx_logs";
 
-  // var collection = context.services.get(serviceName).db(dbName).collection(collName);
-  var collection = client.db(dbName).collection(collName);
+  const voyageAiToken = process.env.VOYAGEAI_TOKEN; // For internal testing
+  // const voyageAiToken = context.environment.values.VOYAGEAI_TOKEN; // For function
+
+  let doc = arg.fullDocument;
+  const { data } = await axios.post('https://api.voyageai.com/v1/embeddings', {
+    "input": [doc.product],
+    "model": "voyage-3-lite"
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Bearer ${voyageAiToken}`
+    }
+  })
+ 
+  const newDoc = {
+    amount: doc.amount,
+    litres: doc.litres,
+    product: doc.product,
+    productEmbedding: data.data[0].embedding
+  };
+
+  // var collection = context.services.get(serviceName).db(dbName).collection(collName); // For function
+  var collection = client.db(dbName).collection(collName); // For internal testing
 
   var insertResult;
   try {
-    insertResult = await collection.insertOne(arg);
+    insertResult = await collection.insertOne(newDoc);
 
   } catch (err) {
     console.log("Error occurred while executing insertOne:", err.message);
@@ -27,7 +48,7 @@ const exportFn = async function (arg) {
     return { error: err.message };
   }
 
-  await client.close();
+  await client.close(); // For internal testing
 
   return { result: insertResult };
 
