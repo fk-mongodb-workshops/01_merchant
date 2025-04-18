2035e9238580d918d0c96a4ab5a4458ab7d7e2e3
Fernando Karnagi <fkarnagi@gmail.com>
2025-04-18 09:03:30 +0800

    Update

--- story_teller/fn_linkaja_customer_refuels.js
+++ story_teller/fn_linkaja_customer_refuels.js
@@ -3,7 +3,20 @@ import { MongoClient } from "mongodb";
 import 'dotenv/config';
 import axios from 'axios';
 
-
+const customerToStory = customerObj => {
+  let ret = `This is story about me. My name is ${customerObj.profile.name}. `;
+  ret = ret + `My total spent until this present is as follow, total amount ${customerObj.to_date.total_spent} Rupiah and ${customerObj.to_date.total_litres} litres. `;
+  ret = ret + `My average spent is as follow, amount ${customerObj.to_date.average_spent} Rupiah and ${customerObj.to_date.average_litres} litres. `;
+  ret = ret + `I mostly visited this gas station ${customerObj.to_date.most_spbu}. `;
+  ret = ret + `I mostly purchased this product ${customerObj.to_date.most_product}. `;
+  ret = ret + `My last refueled amount is ${customerObj.last_refuel.total_spent} Rupiah and ${customerObj.last_refuel.total_litres} litres, at this fuel station ${customerObj.last_refuel.spbu}, on ${moment(customerObj.last_refuel.date, "YYYY-MM-DD HH:mm").format("DD-MMM-YYYY HH:mm")}, this product ${customerObj.last_refuel.product}. `;
+  ret = ret + `The following is my most recent refueling activities: `;
+  for (let i = 0; i < customerObj.refuels.length; i++) {
+    const r = customerObj.refuels[i];
+    ret = ret + `On ${moment(r.date, "YYYY-MM-DD HH:mm").format("DD-MMM-YYYY HH:mm")} at this fuel station ${r.spbu}, refueled ${r.litres} litres of this product ${r.product} as much as ${r.amount}, `;
+  }
+  return ret;
+}
 
 const exportFn = async function (arg) {
   const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.0pftinh.mongodb.net/`;
@@ -25,40 +38,88 @@ const exportFn = async function (arg) {
   // Start of function
   // ---------------------------------------
 
-  var serviceName = "linkaja-dev";
+  const serviceName = "linkaja-dev";
+  const con = context.services.get(serviceName);
 
-  var dbName = "linkaja";
-  var collName = "trx_logs";
+  const dbName = "linkaja";
+  const trxLogCollNm = "trx_logs";
+  const customerCollNm = "customers";
 
   const voyageAiToken = context.environment.values.VOYAGEAI_TOKEN;
 
-  let doc = arg.fullDocument;
-  const { data } = await axios.post('https://api.voyageai.com/v1/embeddings', {
-    "input": [doc.product],
-    "model": "voyage-3-lite"
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      "Authorization": `Bearer ${voyageAiToken}`
-    }
-  })
+  const doc = arg.fullDocument;
 
-  const newDoc = {
-    amount: doc.amount,
-    litres: doc.litres,
-    product: doc.product,
-    productEmbedding: data.data[0].embedding
-  };
-
-  var collection = context.services.get(serviceName).db(dbName).collection(collName);
-
-  var insertResult;
   try {
-    insertResult = await collection.insertOne(newDoc);
+    // Transaction logging
+    const customerColl = con.db(dbName).collection(customerCollNm);
+    const customer = await customerColl.findOne({ "profile.userid": doc.user_id });
+    let newCustomer = {};
+    newCustomer.last_refuel = {
+      spbu: doc.spbu,
+      date: doc.date,
+      amount: doc.amount,
+      product: doc.product,
+      litres: doc.litres
+    }
+
+    const totalSpent = customer.to_date.total_spent || 0;
+    const totalLitres = customer.to_date.total_litres || 0;
+    const totalActivities = customer.to_date.total_activities || 0;
+    newCustomer.to_date = {};
+    newCustomer.to_date.total_spent = doc.amount + totalSpent;
+    newCustomer.to_date.total_litres = doc.litres + totalLitres;
+    newCustomer.to_date.total_activities = totalActivities + 1;
+    newCustomer.to_date.average_spent = (newCustomer.to_date.total_spent / newCustomer.to_date.total_activities).toFixed(0);
+    newCustomer.to_date.average_litres = (newCustomer.to_date.total_litres / newCustomer.to_date.total_activities).toFixed(0);
+    newCustomer.to_date.most_product = doc.product;
+    newCustomer.to_date.most_spbu = doc.spbu;
+
+    newCustomer.profile = {
+      userid: doc.user_id,
+      name: doc.user_name,
+    }
+
+    let refuels = newCustomer.refuels;
+    if (refuels == null) {
+      refuels = []
+    }
+    refuels.push(newCustomer.last_refuel);
+    newCustomer.refuels = refuels;
+
+    const myStory = customerToStory(newCustomer);
+
+    const embeddingData = await axios.post('https://api.voyageai.com/v1/embeddings', {
+      "input": [myStory],
+      "model": "voyage-3-lite"
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${voyageAiToken}`
+      }
+    })
+ 
+    const updateResult = await con.db(dbName).collection(customerCollNm).updateOne({ "profile.userid": doc.user_id },
+      {
+        $set: {
+          to_date: newCustomer.to_date, profile: newCustomer.profile, last_refuel: newCustomer.last_refuel,
+          refuels: newCustomer.refuels, my_story: myStory, my_story_embedding: embeddingData.data.data[0].embedding
+        }
+      },
+      {
+        upsert: true
+      });
 
   } catch (err) {
-    console.log("Error occurred while executing insertOne:", err.message);
+    console.log("Error occurred while executing updating customer record:", err.message);
+    await client.close(); // For internal testing
+    return { error: err.message };
+  }
 
+  try {
+    await con.db(dbName).collection(trxLogCollNm).insertOne(arg);
+  } catch (err) {
+    console.log("Error occurred while inserting trx-log:", err.message);
+    await client.close(); // For internal testing
     return { error: err.message };
   }
 
@@ -66,7 +127,7 @@ const exportFn = async function (arg) {
   // end of function
 
   await client.close(); // For internal testing
-  return { result: insertResult };
+  return { arg };
 };
 
 export { exportFn as execute };
